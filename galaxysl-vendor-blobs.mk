981d46d255675ce9c4d8bc8396564e9e8f627c1c
Dheeraj CVR <cvr.dheeraj@gmail.com>
2013-02-19 15:38:53 +0530

    wifi: use nvs template instead of actual nvs

--- galaxysl-vendor-blobs.mk
+++ galaxysl-vendor-blobs.mk
@@ -117,7 +117,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/galaxysl/proprietary/wifi/firmware/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
     vendor/samsung/galaxysl/proprietary/wifi/firmware/wl127x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
     vendor/samsung/galaxysl/proprietary/wifi/firmware/wl127x-fw-4-sr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
-    vendor/samsung/galaxysl/proprietary/wifi/firmware/wl1271-nvs.bin:system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
+    vendor/samsung/galaxysl/proprietary/wifi/firmware/wl1271-nvs_127x.bin:system/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin
 
 # ril
 PRODUCT_COPY_FILES += \
